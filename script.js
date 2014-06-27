33a15c7ef85a7c90b2069f80f850665c8f8032d0
jezzamon <jerrelunia@gmail.com>
2014-06-27 18:51:29 -0400

    Shopping List app assignment

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    $("#button").click(function() {
+      $(this).effect("bounce");	
+    var addItem = $("input[name=checkListItem]").val();
+      
+    $(".list").append('<div class="item">' + addItem + '</div>') ;
+
+    });
+
+    $(document).on("click", ".item", function() {
+        $(this).remove();
+    });
+    });
